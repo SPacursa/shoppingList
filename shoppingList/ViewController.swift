c4200c6d1d32af1c3867b1998ff39f04785801fd
Sean Pacursa <SPacursa2478@jh-mba158-14.local>
2019-02-13 11:37:29 -0600

    quantity

--- shoppingList/ViewController.swift
+++ shoppingList/ViewController.swift
@@ -17,16 +17,16 @@ class ViewController: UIViewController, UITableViewDataSource {
         super.viewDidLoad()
         tableView.dataSource = self
         
-        let item1 = Item(name: "milk")
-        let item2 = Item(name: "eggs")
+        let item1 = Item(name: "milk", quantity: 3)
+        let item2 = Item(name: "eggs", quantity: 4)
         items = [item1, item2]
-        let item3 = Item(name: "bread")
+        let item3 = Item(name: "bread", quantity: 7)
         items.append(item3)
     }
    
     @IBAction func addNewItemButtonPressed(_ sender: UIBarButtonItem) {
         if let newItemName = newItemTextField.text, newItemName != "" {
-            let newItem = Item(name: newItemName)
+            let newItem = Item(name: newItemName, quantity: 1)
             items.append(newItem)
             tableView.reloadData()
         }
